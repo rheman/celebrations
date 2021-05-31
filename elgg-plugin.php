1d847122d95ef36718d3ae0990a2ef94d9542057
Hermand PESSEK <pessek@bitbucket.org>
2021-05-31 22:01:49 +0000

    First Release

---
+++ elgg-plugin.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+	'bootstrap' => \Pessek\PessekCelebrations\Bootstrap::class,
+	'settings' => [
+		'replaceage' => 'no',
+		'celebrations_birthdate_field' => 'yes',
+		'celebrations_weddingdate_field' => 'yes',
+	],
+	'actions' => [
+		'celebrations/customselection' => [],
+	],
+	'routes' => [
+		'celebrations:celebrations:shorttitle' => [
+			'path' => '/celebrations/celebrations/{month?}/{filterid?}',
+			'resource' => 'celebrations/celebrations',
+		],
+		'celebrations:todaycelebrations:shorttitle' => [
+			'path' => '/celebrations/todaycelebrations/{month?}/{filterid?}/{day?}',
+			'resource' => 'celebrations/todaycelebrations',
+		],
+		'celebrations:celebrations:customselection' => [
+			'path' => '/celebrations/customselection',
+			'resource' => 'celebrations/customselection',
+		],
+	],
+	'widgets' => [
+		'today_celebrations' => [
+			'context' => ['dashboard'],
+		],
+		'next_celebrations' => [
+			'context' => ['dashboard'],
+		],
+		'index_today_celebrations' => [
+			'context' => ['index'],
+		],
+		'index_next_celebrations' => [
+			'context' => ['index'],
+		],
+	]
+];
